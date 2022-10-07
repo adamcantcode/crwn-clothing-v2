4d2856654d598f32f4d8da9a67ea748c69055323
Adam Macaulay <amacaulay.g@gmail.com>
2022-10-07 15:53:46 -0600

    now with useEffect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,34 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    console.log(response);
+    if(response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user)
+    }
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef);
   };
 
   return (
     <div>
       <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign in with google popup</button>
+      <button onClick={logGoogleUser}>Sign in with google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with google Redirect
+      </button>
     </div>
   );
 };
