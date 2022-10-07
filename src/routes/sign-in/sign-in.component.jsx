2500dc7a90964a4687e2eedd2b7be3a15369d3a9
Adam Macaulay <amacaulay.g@gmail.com>
2022-10-07 15:55:13 -0600

    nvm

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-  useEffect(async () => {
-    const response = await getRedirectResult(auth);
-    console.log(response);
-    if(response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user)
-    }
-  }, []);
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -26,9 +14,7 @@ const SignIn = () => {
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>Sign in with google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with google Redirect
-      </button>
+
     </div>
   );
 };
